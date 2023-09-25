6403ca301349d0c7f8325fe59f531e7a7883d8ee
Fan Ni <fan.ni@samsung.com>
2023-09-25 18:03:31 +0100

    hw/mem/cxl_type3: Add support to create DC regions to type3 memory devices
    With the change, when setting up memory for type3 memory device, we can
    create DC regions.
    A property 'num-dc-regions' is added to ct3_props to allow users to pass the
    number of DC regions to create. To make it easier, other region parameters
    like region base, length, and block size are hard coded. If needed,
    these parameters can be added easily.
    
    With the change, we can create DC regions with proper kernel side
    support as below:
    
    region=$(cat /sys/bus/cxl/devices/decoder0.0/create_dc_region)
    echo $region> /sys/bus/cxl/devices/decoder0.0/create_dc_region
    echo 256 > /sys/bus/cxl/devices/$region/interleave_granularity
    echo 1 > /sys/bus/cxl/devices/$region/interleave_ways
    
    echo "dc0" >/sys/bus/cxl/devices/decoder2.0/mode
    echo 0x40000000 >/sys/bus/cxl/devices/decoder2.0/dpa_size
    
    echo 0x40000000 > /sys/bus/cxl/devices/$region/size
    echo  "decoder2.0" > /sys/bus/cxl/devices/$region/target0
    echo 1 > /sys/bus/cxl/devices/$region/commit
    echo $region > /sys/bus/cxl/drivers/cxl_region/bind
    
    Signed-off-by: Fan Ni <fan.ni@samsung.com>

--- hw/mem/cxl_type3.c
+++ hw/mem/cxl_type3.c
@@ -721,6 +721,36 @@ static void ct3d_reg_write(void *opaque, hwaddr offset, uint64_t value,
     }
 }
 
+static int cxl_create_dc_regions(CXLType3Dev *ct3d)
+{
+    int i;
+    uint64_t region_base = 0;
+    uint64_t region_len =  2 * GiB;
+    uint64_t decode_len = 8; /* 8*256MB */
+    uint64_t blk_size = 2 * MiB;
+    CXLDCDRegion *region;
+
+    if (ct3d->hostvmem) {
+        region_base += ct3d->hostvmem->size;
+    }
+    if (ct3d->hostpmem) {
+        region_base += ct3d->hostpmem->size;
+    }
+    for (i = 0; i < ct3d->dc.num_regions; i++) {
+        region = &ct3d->dc.regions[i];
+        region->base = region_base;
+        region->decode_len = decode_len;
+        region->len = region_len;
+        region->block_size = blk_size;
+        /* dsmad_handle is set when creating cdat table entries */
+        region->flags = 0;
+
+        region_base += region->len;
+    }
+
+    return 0;
+}
+
 static bool cxl_setup_memory(CXLType3Dev *ct3d, Error **errp)
 {
     DeviceState *ds = DEVICE(ct3d);
@@ -787,6 +817,10 @@ static bool cxl_setup_memory(CXLType3Dev *ct3d, Error **errp)
         ct3d->cxl_dstate.pmem_size = memory_region_size(pmr);
         ct3d->cxl_dstate.static_mem_size += memory_region_size(pmr);
         g_free(p_name);
+    }
+
+    if (cxl_create_dc_regions(ct3d)) {
+        return false;
     }
 
     return true;
@@ -1108,6 +1142,7 @@ static Property ct3_props[] = {
     DEFINE_PROP_UINT64("sn", CXLType3Dev, sn, UI64_NULL),
     DEFINE_PROP_STRING("cdat", CXLType3Dev, cxl_cstate.cdat.filename),
     DEFINE_PROP_UINT16("spdm", CXLType3Dev, spdm_port, 0),
+    DEFINE_PROP_UINT8("num-dc-regions", CXLType3Dev, dc.num_regions, 0),
     DEFINE_PROP_END_OF_LIST(),
 };
 
