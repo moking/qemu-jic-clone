584ab25e3f5020ed353a2be9adb4187b5271886e
Fan Ni <fan.ni@samsung.com>
2025-04-02 21:49:41 -0700

    cxl_type3: Preparing information sharing between VMs
    Add two data structures for sharing information between multiple VMs.
    The global cci_map_buf is used to provide mappings between cci name and cci
    pointer.
    Each VM has its own cci_map_buf. However, since we expect the two VMs share
    the same configuration, the same CCI name should points to its own CCI.
    On the FM, we need to use the cci pointer to find the cci name.
    While on the test VM, we use the name to find cci pointer for MCTP
    message process.
    The mctp_shared_buffer is used to pass MCTP command information between
    FM and test VM for QMP command process.
    
    Signed-off-by: Fan Ni <fan.ni@samsung.com>

---
+++ include/hw/cxl/cxl_mctp_message.h
@@ -0,0 +1,40 @@
+#ifndef CXL_MCTP_MESSAGE_H_H
+#define CXL_MCTP_MESSAGE_H_H
+#include<stdint.h>
+
+#define MCTP_CXL_MAILBOX_BYTES 512
+#define MCTP_MESSAGE_BUF_NAME "mctp-message-buf"
+
+struct CXLMCTPCommandBuf {
+    /* uint8_t cci_name[64]; */
+    uint8_t command_set;
+    uint8_t command;
+    size_t len_in;
+    size_t len_out;
+    uint8_t payload[MCTP_CXL_MAILBOX_BYTES];
+    uint8_t payload_out[MCTP_CXL_MAILBOX_BYTES];
+    bool bg_started;
+    int ret_val;
+};
+
+typedef struct CXLMCTPCommandBuf CXLMCTPCommandBuf;
+
+struct CXLCCINamePtrMap {
+    char cci_name[64];
+    void *cci_pointer; /* This should be filled by the target VM */
+};
+
+struct CXLCCINamePtrMaps {
+    int num_mappings;
+    struct CXLCCINamePtrMap maps[32];
+};
+
+struct CXLMCTPSharedBuf {
+    /* set to 1 when sent to target VM and wait for 0 as it completes */
+    int status;
+    CXLMCTPCommandBuf command_buf;
+};
+
+typedef struct CXLMCTPSharedBuf CXLMCTPSharedBuf;
+extern struct CXLCCINamePtrMaps *cci_map_buf;
+#endif
